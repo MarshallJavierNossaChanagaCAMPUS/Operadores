b6fac1fc567346377dde587368f72fea178b6be1
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-03-09 08:11:19 -0500

    feat: Operador round

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //Operadores Aritmeticos (libreria matematica)
 
-//Operador floor, devuelve el máximo entero menor o igual a un número.
+//Operador round, la función Math.round retorna el valor de un número redondeado al entero más cercano.
 
-alert(Math.floor(68.1239812498712498781924987124))
+alert(Math.round(3.14159))
+
+alert(Math.round(3.54159))
